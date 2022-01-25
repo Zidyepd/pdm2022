ae1694b5a959501b95ad37f15fb1fb533b3b108f
Zidyepd <78007114+Zidyepd@users.noreply.github.com>
2022-01-25 15:39:21 -0600

    Add files via upload

---
+++ Homework1/example_2_2022_01_25_05_06_46/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  let c = color('violet');
+  fill(250, 40, 66, 100);
+  ellipse(200, 52, 100, 100);
+  
+  fill(127, 63, 191, 120);
+  ellipse(170, 110, 100, 100);
+ 
+  fill(0, 240, 0, 80);
+  ellipse(230, 110, 100, 100);
+}
